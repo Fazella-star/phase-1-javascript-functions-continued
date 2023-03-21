26fb050a4c48f49987e3fcc5cf190d2e66856622
Moringa School <edwardgitau004@gmail.com>
2023-03-21 18:37:15 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(string="roller-skate") {
+     return`This Saturday, I want to ${string}!`;
+}
+  saturdayFun("I want to bathe my dog")
+
+function mondayWork(home="go to the office"){
+   return(`This Monday, I will ${home}.`);
+}
+mondayWork("work from home")
+
+function wrapAdjective(line='*'){
+return innerfunction=function(tickle="special"){
+    return `You are ${line}${tickle}${line}!`
+}
+}
+ 
